a03a8b5f6c8dcf8f3ec0727488d395a7eae0190e
Tahseen Malik <hello@tahseen.me>
2018-07-04 14:14:54 +0100

    test: Add a simple test server

---
+++ test/server.js
@@ -0,0 +1,12 @@
+'use strict'
+const path = require('path')
+const { createServer } = require('..')
+
+const app = createServer({
+  ethereumJsonRPC: 'http://localhost:8545',
+  networkConfigPath: path.resolve(__dirname, './fixtures/config.json')
+})
+
+app.listen(3001, () => {
+  console.log('Listening on port 3001...')
+})
