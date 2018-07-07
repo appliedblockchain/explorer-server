efa71ca901f0b386cddc2fb77f5483978702d2a8
Tahseen Malik <hello@tahseen.me>
2018-07-07 19:41:41 +0100

    refactor: Improve createFixedStack algorithm

--- server/utils/createFixedStack.js
+++ server/utils/createFixedStack.js
@@ -1,5 +1,4 @@
 'use strict'
-const { times, isFunction } = require('lodash')
 const validate = require('ow')
 
 /** FIFO (First in first out) Data structure with fixed size */
@@ -8,29 +7,38 @@ const validate = require('ow')
 const createFixedStack = (size, defaultVal) => {
   validate(size, validate.number.positive.integer)
 
-  /* @NOTE: [newestItem...oldestItem] */
-  const stack = times(size).map(
-    isFunction(defaultVal) ? defaultVal : () => defaultVal
-  )
+  let newestIdx = 0
+  const stack = Array(size).fill(defaultVal)
+
+  const retrieve = (total = 1) => {
+    validate(total, validate.number.positive.integer.lessThanOrEqual(size))
+
+    const items = []
+    for (let i = newestIdx; items.length < total; i = (i + 1) % size) {
+      const curr = stack[i]
+      items.push(curr)
+    }
+
+    return items
+  }
+
+  const push = (...items) => {
+    const startIdx = Math.max(items.length - size, 0)
+
+    for (let i = startIdx; i < items.length; i += 1) {
+      const currItem = items[i]
+      const oldestIdx = (newestIdx + size - 1) % size
+
+      stack[oldestIdx] = currItem
+      newestIdx = oldestIdx
+    }
+
+    return retrieve(size)
+  }
 
   const api = {
-    push(...items) {
-      const toAdd = items.length <= size
-        ? items
-        : items.slice(items.length - size)
-
-      times(toAdd.length, stack.pop.bind(stack))
-      toAdd.forEach(item => stack.unshift(item))
-
-      return stack
-    },
-
-    retrieve(total = 1) {
-      validate(total, validate.number.positive.integer.lessThanOrEqual(size))
-
-      return stack.slice(0, total)
-    },
-
+    retrieve,
+    push,
     size() {
       return size
     }
